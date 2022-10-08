e95fe817c45d26f925a81cb42683f3ef6120afe3
SSulaberidze <ssulberidze@gmail.com>
2022-10-08 14:56:43 +0400

    First update

---
+++ script.js
@@ -0,0 +1,82 @@
+// // სავარჯიშო 1
+// let array = [5, 25, 89, 120, 36];
+// // array.push('javascript', 'python');
+// // array.unshift('html', 'css');
+// for (let item in array){
+//     console.log(item);
+// }
+// // array.shift() && array.pop();
+
+// console.log(array);
+
+
+
+// // სავარჯიშო 2
+// let fruits = ['ფორთოხალი', 'ბანანი', 'მსხალი'];
+// // for (let items in fruits){
+// //     console.log(items);
+// // }
+// // fruits.unshift('საზამთრო') && fruits.push('ვაშლი', 'ანანსი');
+// // console.log(fruits.length);
+// // fruits.splice(2,0, 'მანგო');
+// // let newFruits = fruits.slice (1,2);
+// // console.log(newFruits);
+
+// console.log(fruits);
+
+
+
+// // სავარჯიშო 5
+// let languages = ['html', 'css', 'javascript', 'python', 'php'];
+// let newLanguages = languages.filter ((items) => items.length > 3);
+ 
+// console.log(newLanguages);
+
+
+
+// // სავარჯიშო 7
+// let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+// let newWords = words.filter ((items) => items.includes ('m') || items.includes ('M'));
+
+// console.log(newWords);
+
+
+
+// // სავარჯიშო 8
+// let arr1 = [1, 2];
+// let arr2 = [3, 4];
+// let arr3 = [5, 6];
+// let unifiedArray = arr1.concat(arr2).concat(arr3);
+
+// console.log(unifiedArray);
+
+
+
+// // სავარჯიშო 10
+// let numbers = [-2, 5, 10, -5, -1, 1, 3, 4, -15];
+// let newNumbers = numbers.filter ((items) => items > 0);
+
+// console.log(newNumbers);
+
+
+
+// // სავარჯიშო 11
+// let arr = [-1, -2, -3, 4].some((items) => items > 0);
+
+// console.log(arr);
+
+
+
+// // სავარჯიშო 12 გადასამოწმებელი
+// let array = [23,45,32,5,87,7,3,98];
+// let newArray = array.sort ((x,y) => x-y)[0];
+
+// console.log(newArray);
+
+
+
+// // სავარჯიშო 13
+// let array =[1, 2, 3, 4, 5];
+// array.splice (3,0, 'a', 'b', 'c');
+
+// console.log(array);
