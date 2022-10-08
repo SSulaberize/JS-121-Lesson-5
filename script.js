17b706d7d89e236285ab5fbd54643f684bd97d52
SSulaberidze <ssulberidze@gmail.com>
2022-10-08 22:28:26 +0400

    final update

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // // სავარჯიშო 1
 // let array = [5, 25, 89, 120, 36];
-// // array.push('javascript', 'python');
-// // array.unshift('html', 'css');
-// for (let item in array){
-//     console.log(item);
-// }
-// // array.shift() && array.pop();
+// array.push('javascript', 'python');
+// array.unshift('html', 'css');
+// console.log(array.length);
+// array.shift() && array.pop();
 
 // console.log(array);
 
@@ -13,30 +11,49 @@
 
 // // სავარჯიშო 2
 // let fruits = ['ფორთოხალი', 'ბანანი', 'მსხალი'];
-// // for (let items in fruits){
-// //     console.log(items);
-// // }
-// // fruits.unshift('საზამთრო') && fruits.push('ვაშლი', 'ანანსი');
-// // console.log(fruits.length);
-// // fruits.splice(2,0, 'მანგო');
-// // let newFruits = fruits.slice (1,2);
-// // console.log(newFruits);
-
+// console.log(fruits.length);
+// fruits.unshift('საზამთრო') && fruits.push('ვაშლი', 'ანანსი');
+// console.log(fruits.length);
+// fruits.splice(2,0, 'მანგო');
+// let newFruits = fruits.slice (1,2);
+// console.log(newFruits);
 // console.log(fruits);
+
+
+
+// // სავარჯიშო 3
+// let array = [12, 25, 3, 6, 8, 14, 7, 23];
+// let newArray = array.map(items => items/3);
+
+// console.log(newArray);
+
+
+
+// // სავარჯიშო 4
+// let array = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
+// let newArray = array.filter(items => (typeof items == 'string'));
+
+// console.log(newArray);
 
 
 
 // // სავარჯიშო 5
 // let languages = ['html', 'css', 'javascript', 'python', 'php'];
-// let newLanguages = languages.filter ((items) => items.length > 3);
+// let newLanguages = languages.filter (items => items.length > 3);
  
 // console.log(newLanguages);
 
 
 
+// სავარჯიშო 6
+// let item = [12, 'google', 32, null, 'yahoo', 25];
+// let newItem = item.map(elements => elements)
+
+
+
 // // სავარჯიშო 7
 // let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
-// let newWords = words.filter ((items) => items.includes ('m') || items.includes ('M'));
+// let newWords = words.filter (items => items.includes ('m') || items.includes ('M'));
 
 // console.log(newWords);
 
@@ -52,24 +69,35 @@
 
 
 
+// // სავარჯიშო 9
+// let arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+// arr.forEach(function(numbers){
+//     sum += numbers;
+//     return sum;
+// })
+// console.log(sum);
+
+
+
 // // სავარჯიშო 10
 // let numbers = [-2, 5, 10, -5, -1, 1, 3, 4, -15];
-// let newNumbers = numbers.filter ((items) => items > 0);
+// let newNumbers = numbers.filter (items => items > 0);
 
 // console.log(newNumbers);
 
 
 
 // // სავარჯიშო 11
-// let arr = [-1, -2, -3, 4].some((items) => items > 0);
+// let arr = [-1, -2, -3, 4].some(items => items > 0);
 
 // console.log(arr);
 
 
 
-// // სავარჯიშო 12 გადასამოწმებელი
+// // // სავარჯიშო 12
 // let array = [23,45,32,5,87,7,3,98];
-// let newArray = array.sort ((x,y) => x-y)[0];
+// let newArray = array.sort ((x,y) => y-x)[array.length-1];
 
 // console.log(newArray);
 
